f4116d781a5461202440abc6cf3605fe77272c52
yashwandhare <wandhareyash7@gmail.com>
2026-01-10 13:15:16 +0530

    Add file picker and path validation

--- converters/image.py
+++ converters/image.py
@@ -1,3 +1,10 @@
+from PIL import Image
+
+
 def conv_image(path):
     print("image convert called")
     print(path)
+    with Image.open(path) as img:
+        print(img.filename)
+        print(img.format)
+        print(img.size)
